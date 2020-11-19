cdfcd4d48f48d3d9c5aa84668fa30e3bb3fef695
jacheampong <jacheampong@gmail.com>
2020-11-19 12:14:52 -0500

    added core components

---
+++ src/Artists.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Artists extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Artists</h3>
+            </div>
+        )
+    }
+}
---
+++ src/Tracks.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Tracks extends Component {
+    render() {
+        return(
+            <div>
+                <h3>Tracks</h3>
+            </div>
+        )
+    }
+}
