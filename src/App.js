d12fa11e9c7bd0c986b79c348e24af3146d41d60
jacheampong <jacheampong@gmail.com>
2020-11-19 12:15:59 -0500

    initial App update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
+import MUSIX from './MUSIX'
+import Artists from './Artists'
+import Tracks from './Tracks'
 
 export default class App extends Component {
+
   render() {
     return(
       <div>
         <h1>React Music Match!</h1>
+        <form onSubmit={this.handleSearch}>
+          <label>
+            <input name="searchText" type="text" />
+          </label>
+          <button>SEARCH</button>
+        </form>
+        <Artists artist={MUSIX.artists} />
+        <Tracks tracks={MUSIX.artists} />
       </div>
     )
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    console.log('Handle Search', e.target.searchText.value)
+    
   }
 
 }
