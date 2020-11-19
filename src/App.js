1899e7077f19a12342c96144583be63e38894c9d
jacheampong <jacheampong@gmail.com>
2020-11-19 14:09:39 -0500

    added dropdown for search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Tracks from './Tracks'
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchType: 'artist',
+      searchText: '',
+    }
+  }
+
   render() {
     return(
       <div>
@@ -13,18 +22,32 @@ export default class App extends Component {
           <label>
             <input name="searchText" type="text" />
           </label>
+          <select value={this.state.searchType} onChange={this.handleChange}>
+            <option value="artist">Artists</option>
+            <option value="track">Tracks</option>
+            <option value="album" disabled>Albums</option>
+            <option value="lyric" disabled>Lyrics</option>
+          </select>
           <button>SEARCH</button>
         </form>
         <Artists artist={MUSIX.artists} />
-        <Tracks tracks={MUSIX.artists} />
+        <Tracks tracks={MUSIX.tracks} />
       </div>
     )
   }
 
   handleSearch = (e) => {
     e.preventDefault();
-    console.log('Handle Search', e.target.searchText.value)
+    console.log('Handle Text: ', e.target.searchText.value)
+    console.log('Handle Type: ', this.state.searchType)
     
+  }
+
+  handleChange = (event) => {
+    console.log('Dropdown: ', event.target.value)
+    this.setState({
+      searchType: event.target.value
+    });
   }
 
 }
